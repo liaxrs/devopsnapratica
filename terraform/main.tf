cd9fa8bbbffc82386052b3c216e3086fc77aedb9
liaxrs <148290984+liaxrs@users.noreply.github.com>
2026-08-24 14:36:33 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "servidor_devops" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Servidor-DevOps"
+  }
+}
