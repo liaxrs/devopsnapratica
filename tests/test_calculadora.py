036318f2e858e23fead3dbefc3d3739d982da8ab
liaxrs <148290984+liaxrs@users.noreply.github.com>
2026-08-24 14:52:42 -0300

    Update test_calculadora.py

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from calculadora import soma
 
 def test_soma():
-    assert soma (2, 3) == 5
+    assert soma(2, 3) == 5
